999c8164fc9c36de7367c51b1aa2b5047b21c55f
tariqeddoumi <139554666+tariqeddoumi@users.noreply.github.com>
2026-04-14 16:08:51 +0100

    Upload lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,33 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatMAD(montant: number): string {
+  return new Intl.NumberFormat("fr-MA", {
+    style: "currency",
+    currency: "MAD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(montant);
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(date);
+}
+
+export function formatDateTime(date: Date): string {
+  return new Intl.DateTimeFormat("fr-FR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date);
+}
