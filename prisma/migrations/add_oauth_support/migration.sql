07dd69cb4a0cd2c04daff09f0229c73c74ce6ac6
tariqeddoumi <139554666+tariqeddoumi@users.noreply.github.com>
2026-04-14 16:01:51 +0100

    Delete prisma/migrations/add_oauth_support/migration.sql

--- prisma/migrations/add_oauth_support/migration.sql
+++
@@ -1,4 +0,0 @@
--- Add OAuth support columns to pf_scoring_users table
-ALTER TABLE "pf_scoring_users" ADD COLUMN "oauthProvider" TEXT;
-ALTER TABLE "pf_scoring_users" ADD COLUMN "oauthId" TEXT;
-ALTER TABLE "pf_scoring_users" ADD COLUMN "avatar" TEXT;
